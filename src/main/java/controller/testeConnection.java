e2ea7f2a3a7035848e52b79a85938605bf035fa2
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2026-02-25 15:30:12 -0300

    :test_tube: test: testando a conexão com o banco de dados

---
+++ src/main/java/controller/testeConnection.java
@@ -0,0 +1,44 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Dao;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class testeConnection
+ */
+@WebServlet("/testeConnection")
+public class testeConnection extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testeConnection() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Dao dao = new Dao();
+		
+		try {
+			dao.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
